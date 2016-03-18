b4916d91c4c91b1aced6fddc71382882eb51a866
muji <freeformsystems@gmail.com>
2016-03-18 13:53:19 +0800

    Use mkref program for readme build.

--- doc/readme/usage.js
+++ doc/readme/usage.js
@@ -1,10 +1,12 @@
 var doc = require('../../index')
   , pi = require('mkpi')
   , gen = require('mkgen')
+  , ref = require('mkref')
   , out = require('mkout');
 
 doc('doc/readme.md')
   .pipe(pi())
   .pipe(gen())
+  .pipe(ref())
   .pipe(out())
   .pipe(process.stdout);
