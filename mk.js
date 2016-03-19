5ef4bd677ee57dddee7694d137803cf61b4d36a4
muji <freeformsystems@gmail.com>
2016-03-19 21:30:04 +0800

    Use mk to build the readme.

--- mk.js
+++ mk.js
@@ -1,20 +1,23 @@
 var mk = require('mktask');
 
+// @task readme build the api docs.
 function api(cb) {
   console.dir('api called');
   cb();
 }
 
-
+// @task readme build the readme file.
 function readme() {
   mk.doc('doc/readme.md')
     .pipe(this.pi())
     .pipe(this.msg())
     .pipe(this.ref())
+    .pipe(this.abs())
     .pipe(this.out())
-    .pipe(mk.dest('README.md.log'));
+    .pipe(mk.dest('README.md'));
 }
 
+// @task readme build the readme file.
 function main(cb) {
   console.dir('main called');
   cb();
