b7771e600c812a3f145a42a31ea7d912589d369e
muji <freeformsystems@gmail.com>
2016-03-20 20:16:22 +0800

    Draft mkfilter executable.

--- cli/filter.js
+++ cli/filter.js
@@ -1,23 +1,47 @@
 var filter = require('mkfilter')
   , ast = require('mkast')
   , Node = ast.Node
+  , types = Node.types
   , parser = require('cli-argparse')
   , utils = require('./util')
+  , keys = {}
   , options = {
+    '-i, --invert': 'Invert the filter'
+  }
+  , append = {
       '-h, --help': 'Display this help and exit',
       '--version': 'Print the version and exit'
     }
+  , filters = {}
+  , k
   , hints = {
       options: [
       ],
       flags: [
+        '--invert',
         '--help'
       ],
       alias: {
+        '-i --invert': 'invert',
         '-h --help': 'help'
-      }
+      },
+      camelcase: false
     }
   , pkg = require('mkfilter/package.json');
+
+//
+types.forEach(function(type) {
+  filters[type] = false;
+  var key = type.replace(/_/g, '-')
+    , nm = '--' + key;
+  keys[key] = type;
+  options[nm] = 'Filter ' + type.replace(/_/g, ' ') + ' nodes';
+  hints.flags.push(nm);
+})
+
+for(k in append) {
+  options[k] = append[k];
+}
 
 /**
  *  Filter nodes by type.
@@ -41,6 +65,28 @@ function cli(argv, cb) {
   }else if(args.flags.version) {
     utils.version(pkg);
     return cb();
+  }
+
+  var k
+    , type;
+
+  // check args for switches and update filters
+  for(k in keys) {
+    if(args.flags[k]) {
+      type = keys[k];
+      filters[type] = true;
+    } 
+  }
+
+  // invert flags
+  if(args.flags.invert) {
+    for(k in filters) {
+      filters[k] = !filters[k];
+    } 
+  }
+
+  for(k in filters) {
+    opts[k] = filters[k];
   }
 
   filter(opts, cb);
