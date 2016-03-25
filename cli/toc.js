54b1e3bd75773327daf7aa315e36e4097d9224d3
muji <freeformsystems@gmail.com>
2016-03-25 14:52:31 +0800

    Support --title and --level (mktoc).

--- cli/toc.js
+++ cli/toc.js
@@ -2,18 +2,24 @@ var toc = require('mktoc')
   , parser = require('cli-argparse')
   , utils = require('./util')
   , options = {
+      '-t, --title=[TITLE]': 'Set initial heading',
+      '-l, --level=[NUM]': 'Set initial heading level',
       '-s, --standalone': 'Create standalone index',
       '-h, --help': 'Display this help and exit',
       '--version': 'Print the version and exit'
     }
   , hints = {
       options: [
+        '-t',
+        '-l'
       ],
       flags: [
         '--standalone',
         '--help'
       ],
       alias: {
+        '-t --title': 'title',
+        '-l --level': 'level',
         '-s --standalone': 'standalone',
         '-h --help': 'help'
       }
@@ -31,10 +37,18 @@ function cli(argv, cb) {
   }
 
   var args = parser(argv, hints)
-    , opts = {
+    , level = parseInt(args.options.level) || 1;
+
+  if(level < 1) {
+    level = 1; 
+  }
+
+  var opts = {
         input: process.stdin, 
         output: process.stdout,
-        standalone: args.flags.standalone
+        standalone: args.flags.standalone,
+        title: args.options.title,
+        level: level
       };
 
   if(args.flags.help) {
