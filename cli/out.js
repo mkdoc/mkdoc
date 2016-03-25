eea182638518787b240a1b622b6c92104d18f290
muji <freeformsystems@gmail.com>
2016-03-25 12:47:25 +0800

    Output types are alphabetical (mkout).

--- cli/out.js
+++ cli/out.js
@@ -5,10 +5,10 @@ var fs = require('fs')
   , options = {
       '-o, --output=[FILE]': 'Write output to FILE (default: stdout)',
       '-H, --html': 'Set output renderer to HTML',
-      '-x, --xml': 'Set output renderer to XML',
+      '-j, --json': 'Set output renderer to JSON',
       '-m, --man': 'Set output renderer to MAN',
       '-t, --text': 'Set output renderer to TEXT',
-      '-j, --json': 'Set output renderer to JSON',
+      '-x, --xml': 'Set output renderer to XML',
       '-y, --yaml': 'Set output renderer to YAML',
       '-Y, --yaml-full': 'Do not compact YAML output',
       '-n, --noop': 'Pass through input JSON',
