613cc26f744629ec7f8020a8fd1d5f76fd7a4d54
muji <freeformsystems@gmail.com>
2016-03-26 17:30:55 +0800

    Update usage example.

--- doc/readme/usage.js
+++ doc/readme/usage.js
@@ -1,12 +1,16 @@
 var doc = require('../../index')
   , pi = require('mkpi')
-  , msg = require('mkmsg')
   , ref = require('mkref')
+  , abs = require('mkabs')
+  , msg = require('mkmsg')
+  , toc = require('mktoc')
   , out = require('mkout');
 
 doc('doc/readme.md')          // read markdown source document
   .pipe(pi())                 // parse processing instructions, includes etc.
   .pipe(ref())                // include link references
+  .pipe(abs())                // make links absolute
   .pipe(msg())                // append generator message
+  .pipe(toc())             // create and inject table of contents list
   .pipe(out())                // convert abstract syntax tree to markdown
   .pipe(process.stdout);      // print the document
