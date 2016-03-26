2191903d9984540c7761a60f91e77c33e0853317
muji <freeformsystems@gmail.com>
2016-03-26 16:56:20 +0800

    Draft mklevel program.

---
+++ cli/level.js
@@ -0,0 +1,47 @@
+var level = require('mklevel')
+  , parser = require('cli-argparse')
+  , utils = require('./util')
+  , options = {
+      '-h, --help': 'Display this help and exit',
+      '--version': 'Print the version and exit'
+    }
+  , hints = {
+      options: [
+      ],
+      flags: [
+        '--help'
+      ],
+      alias: {
+        '-h --help': 'help'
+      }
+    }
+  , pkg = require('mklevel/package.json');
+
+/**
+ *  Resolve link levelerences.
+ */
+function cli(argv, cb) {
+
+  if(typeof argv === 'function') {
+    cb = argv;
+    argv = null;
+  }
+
+  var args = parser(argv, hints)
+    , opts = {
+        input: process.stdin, 
+        output: process.stdout
+      };
+
+  if(args.flags.help) {
+    utils.usage(pkg, options);
+    return cb();
+  }else if(args.flags.version) {
+    utils.version(pkg);
+    return cb();
+  }
+
+  level(opts, cb);
+}
+
+module.exports = cli;
