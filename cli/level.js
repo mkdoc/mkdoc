ef5d3e765bee569c1d52589340812ab4da44639a
muji <freeformsystems@gmail.com>
2016-03-26 17:08:52 +0800

    Implement mklevel options.

--- cli/level.js
+++ cli/level.js
@@ -2,23 +2,38 @@ var level = require('mklevel')
   , parser = require('cli-argparse')
   , utils = require('./util')
   , options = {
+      '-1=[NUM]': 'Modify level 1 headings by NUM',
+      '-2=[NUM]': 'Modify level 2 headings by NUM',
+      '-3=[NUM]': 'Modify level 3 headings by NUM',
+      '-4=[NUM]': 'Modify level 4 headings by NUM',
+      '-5=[NUM]': 'Modify level 5 headings by NUM',
+      '-6=[NUM]': 'Modify level 6 headings by NUM',
+      '-a, --all=[NUM]': 'Modify all headings by NUM',
       '-h, --help': 'Display this help and exit',
       '--version': 'Print the version and exit'
     }
   , hints = {
       options: [
+        '-a',
+        '-1',
+        '-2',
+        '-3',
+        '-4',
+        '-5',
+        '-6'
       ],
       flags: [
         '--help'
       ],
       alias: {
+        '-a --all': 'all',
         '-h --help': 'help'
       }
     }
   , pkg = require('mklevel/package.json');
 
 /**
- *  Resolve link levelerences.
+ *  Modify heading levels.
  */
 function cli(argv, cb) {
 
@@ -31,7 +46,11 @@ function cli(argv, cb) {
     , opts = {
         input: process.stdin, 
         output: process.stdout
-      };
+      }
+    , levels = []
+    , i
+    , num = 6
+    , val;
 
   if(args.flags.help) {
     utils.usage(pkg, options);
@@ -40,6 +59,28 @@ function cli(argv, cb) {
     utils.version(pkg);
     return cb();
   }
+
+  // propagate all option first
+  if(args.options.all) {
+    val = parseInt(args.options.all) || 0;
+  }
+
+  for(i = 0;i < num;i++) {
+    // zero fill first
+    levels[i] = 0;
+
+    // update with --all when specified
+    if(val !== undefined) {
+      levels[i] = val;
+    }
+
+    // got a specific level flag
+    if(args.options[i + 1] !== undefined) {
+      levels[i] = parseInt(args.options[i + 1]) || 0;
+    }
+  }
+
+  opts.levels = levels;
 
   level(opts, cb);
 }
