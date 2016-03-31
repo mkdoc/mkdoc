84a7610eb145196a348f46ffc5b33960cf6d0e0f
muji <freeformsystems@gmail.com>
2016-03-31 13:20:30 +0800

    Add task to build json program descriptors.

--- mkdoc.js
+++ mkdoc.js
@@ -2,21 +2,8 @@ var fs = require('fs')
   , path = require('path')
   , mk = require('mktask');
 
-// @task api build the api docs.
-function api(cb) {
-  mk.api(
-    ['index.js'],
-    {
-      stream: mk.dest('API.md'),
-      heading: 'API'
-    }, cb);
-}
-
-// @task help build the cli help files.
-function help(cb) {
-  var src = 'doc/cli'
-    , out = 'doc/help'
-    , files = fs.readdirSync(src);
+function bin(type, ext, src, out, buffer, cb) {
+  var files = fs.readdirSync(src);
 
   function next(err) {
     if(err) {
@@ -30,20 +17,52 @@ function help(cb) {
 
     var source = path.join(src, file)
       , name = file.replace(/\.md$/, '')
-      , dest = name + '.txt'
+      , dest = name + '.' + ext
       , pkg;
 
     pkg = require((name === 'mk' ? 'mktask' : name) + '/package.json');
     dest = path.join(out, dest);
 
-    mk.doc(source)
-      .pipe(mk.cli.src())
-      .pipe(mk.cli.dest({type: 'help', pkg: pkg}))
+    var opts = {type: type, pkg: pkg};
+
+    var stream = mk.doc(source);
+
+    // prevent circular references when json output type
+    if(type === mk.cli.types.json) {
+      stream = stream.pipe(mk.ast.convert());
+    }
+      
+    stream.pipe(mk.cli.src(opts))
+      .pipe(mk.cli.dest(opts))
       .pipe(mk.dest(dest))
       .on('finish', next);
   }
 
   next();
+}
+
+// @task api build the api docs.
+function api(cb) {
+  mk.api(
+    ['index.js'],
+    {
+      stream: mk.dest('API.md'),
+      heading: 'API'
+    }, cb);
+}
+
+// @task help build the cli help files.
+function help(cb) {
+  var src = 'doc/cli'
+    , out = 'doc/help';
+  bin('help', 'txt', src, out, false, cb);
+}
+
+// @task cli build the cli definition files.
+function cli(cb) {
+  var src = 'doc/cli'
+    , out = 'doc/cli';
+  bin('json', 'json', src, out, true, cb);
 }
 
 // @task readme build the readme file.
@@ -62,5 +81,6 @@ function readme(cb) {
 }
 
 mk.task(api);
+mk.task(cli);
 mk.task(help);
 mk.task(readme);
