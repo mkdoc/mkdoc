a02aababdece120e2fce33a0ba7adf0c3c3be7a2
muji <freeformsystems@gmail.com>
2016-03-31 10:52:54 +0800

    Support -p | --package (mkcli).

--- cli/cli.js
+++ cli/cli.js
@@ -1,19 +1,23 @@
-var cli = require('mkcli')
+var path = require('path')
+  , cli = require('mkcli')
   , parser = require('cli-argparse')
   , utils = require('./util')
   , options = {
+      '-p, --package=[FILE]': 'Use package descriptor',
       '-t, --type=[TYPE]': 'Output renderer type',
       '-h, --help': 'Display this help and exit',
       '--version': 'Print the version and exit'
     }
   , hints = {
       options: [
-        '-t'
+        '-t',
+        '-p'
       ],
       flags: [
         '--help'
       ],
       alias: {
+        '-p --package': 'pkg',
         '-t --type': 'type',
         '-h --help': 'help'
       }
@@ -30,11 +34,27 @@ function prg(argv, cb) {
     argv = null;
   }
 
+  // user defined package.json with additional meta data
   var args = parser(argv, hints)
-    , opts = {
+    , pack
+    , pth = args.options.pkg;
+
+  if(pth) {
+    if(!/^\//.test(pth)) {
+      pth = path.join(process.cwd(), pth);
+    }
+    try {
+      pack = require(pth);
+    }catch(e) {
+      return cb(e); 
+    }
+  }
+
+  var opts = {
         input: process.stdin, 
         output: process.stdout,
-        type: args.options.type || cli.types.json
+        type: args.options.type || cli.types.json,
+        pkg: pack
       };
 
   if(args.flags.help) {
