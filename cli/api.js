a5474d50a1496680c29471ab1a724802b05b252b
muji <freeformsystems@gmail.com>
2016-03-31 19:52:11 +0800

    Use program runner (mkapi).

--- cli/api.js
+++ cli/api.js
@@ -1,90 +1,82 @@
 var fs = require('fs')
-  , parser = require('cli-argparse')
-  , parse = require('mkapi')
+  , path = require('path')
+  , api = require('mkapi')
+  , bin = require('mkcli')
+  , def = require('../doc/cli/mkapi.json')
   , pkg = require('mkapi/package.json')
-  , utils = require('./util')
-  //, options = {
-      //synopsis: '[options] [files...]',
-      //'-o, --output=[FILE]': 'Write output to FILE (default: stdout)',
-      //'-t, --title=[VAL]': 'Title for initial heading',
-      //'-l, --level=[NUM]': 'Initial heading level (default: 1)',
-      //'-L, --lang=[LANG]':
-        //'Language for fenced code blocks (default: javascript)',
-      //'-i, --indent=[NUM]': 'Number of spaces for JSON (default: 2)',
-      //'-a, --ast': 'Print AST as JSON',
-      //'--[no]-private': 'Enable or disable private symbols',
-      //'--[no]-protected': 'Enable or disable protected symbols',
-      //'-h, --help': 'Display this help and exit',
-      //'--version': 'Print the version and exit'
-    //}
-  , hints = {
-      options: [
-        '-l', '-t', '-o', '-i', '-L'
-      ],
-      flags: [
-        '--ast', '--help'
-      ],
-      alias: {
-        '-a --ast': 'ast',
-        '-l --level': 'level',
-        '-L --lang': 'lang',
-        '-t --title': 'title',
-        '-i --indent': 'indent',
-        '-o --output': 'output',
-        '-h --help': 'help'
-      }
-    }
-  , opts = {conf: {include: {}}};
+  , prg = bin.load(def, pkg);
 
+/**
+ *  @name mkapi
+ *  @cli doc/cli/mkapi.md
+ */
 function cli(argv, cb) {
+
   if(typeof argv === 'function') {
     cb = argv;
     argv = null;
   }
 
-  var args = parser(argv, hints);
+  var opts = {
+      input: process.stdin, 
+      output: process.stdout,
+      conf: {include: {}}
+    }
+    , runtime = {
+        base: path.normalize(path.join(__dirname, '..')),
+        target: opts,
+        hints: prg,
+        help: {
+          file: 'doc/help/mkapi.txt'
+        },
+        version: pkg,
+        plugins: [
+          require('mkcli/plugin/hints'),
+          require('mkcli/plugin/argv'),
+          require('mkcli/plugin/help'),
+          require('mkcli/plugin/version')
+        ]
+      };
 
-  if(args.flags.help) {
-    return cb(null, utils.help('doc/help/mkapi.txt'));
-  }else if(args.flags.version) {
-    return cb(null, utils.version(pkg));
-  }
-
-  if(!args.unparsed.length) {
-    return cb(new Error('no files specified'));
-  }
-
-  if(args.options.level && !parseInt(args.options.level)) {
-    return cb(new Error('--level must be an integer'));
-  }
-
-  opts.level = parseInt(args.options.level);
-  opts.ast = args.flags.ast;
-  opts.indent = parseInt(args.options.indent);
-  opts.lang = args.options.lang;
-
-  if(args.options.output) {
-    opts.stream = fs.createWriteStream(args.options.output); 
-  }
-
-  if(args.options.title !== undefined) {
-    opts.heading = args.options.title;
-  }
-
-  if(args.flags.private !== undefined) {
-    opts.conf.include.private = args.flags.private;
-  }
-
-  if(args.flags.protected !== undefined) {
-    opts.conf.include.protected = args.flags.protected;
-  }
-
-  parse(args.unparsed, opts, function(err) {
+  prg.run(argv, runtime, function parsed(err) {
     if(err) {
       return cb(err); 
     }
-    cb(null);
-  });
+
+    if(!this.unparsed.length) {
+      return cb(new Error('no files specified'));
+    }
+
+    if(this.level && !parseInt(this.level)) {
+      return cb(new Error('--level must be an integer'));
+    }
+
+    this.level = parseInt(this.level);
+    this.indent = parseInt(this.indent);
+
+    if(typeof this.output === 'string') {
+      this.stream = fs.createWriteStream(this.output); 
+    }
+
+    if(this.title !== undefined) {
+      this.heading = this.title;
+    }
+
+    if(this.private !== undefined) {
+      this.conf.include.private = this.private;
+    }
+
+    if(this.protected !== undefined) {
+      this.conf.include.protected = this.protected;
+    }
+
+    api(this.unparsed, this, function(err) {
+      if(err) {
+        return cb(err); 
+      }
+      cb(null);
+    });
+  })
 }
 
 module.exports = cli;
