38832e0fc6e4c17e3bf41afc322bbbefa60f0003
muji <freeformsystems@gmail.com>
2016-03-31 20:49:02 +0800

    Use program runner (mkfilter).

--- cli/filter.js
+++ cli/filter.js
@@ -1,80 +1,82 @@
-var filter = require('mkfilter')
+var path = require('path')
+  , filter = require('mkfilter')
+  , bin = require('mkcli')
   , ast = require('mkast')
   , Node = ast.Node
-  , parser = require('cli-argparse')
-  , utils = require('./util')
+  , types = Node.types.concat(Node.extensions)
+  , def = require('../doc/cli/mkfilter.json')
+  , pkg = require('mkfilter/package.json')
+  , prg = bin.load(def, pkg)
   , keys = {}
   , filters = {}
-  , hints = {
-      options: [
-      ],
-      flags: [
-        '--invert',
-        '--help'
-      ],
-      alias: {
-        '-i --invert': 'invert',
-        '-h --help': 'help'
-      },
-      camelcase: false
-    }
-  , pkg = require('mkfilter/package.json');
 
-// update hints
-var types = Node.types.concat(Node.extensions);
 types.forEach(function(type) {
   filters[type] = false;
-  var key = type.replace(/_/g, '-')
-    , nm = '--' + key;
-  keys[key] = type;
-  hints.flags.push(nm);
+  var key = type.replace(/_/g, '-');
+  keys[prg.camelcase(key)] = type;
 })
 
 /**
- *  Filter nodes by type.
+ *  @name mkfilter
+ *  @cli doc/cli/mkfilter.md
  */
-function cli(argv, cb) {
+function main(argv, cb) {
 
   if(typeof argv === 'function') {
     cb = argv;
     argv = null;
   }
 
-  var args = parser(argv, hints)
-    , opts = {
-        input: process.stdin, 
-        output: process.stdout
+  var opts = {
+      input: process.stdin, 
+      output: process.stdout
+    }
+    , runtime = {
+        base: path.normalize(path.join(__dirname, '..')),
+        target: opts,
+        hints: prg,
+        help: {
+          file: 'doc/help/mkfilter.txt'
+        },
+        version: pkg,
+        plugins: [
+          require('mkcli/plugin/hints'),
+          require('mkcli/plugin/argv'),
+          require('mkcli/plugin/help'),
+          require('mkcli/plugin/version')
+        ]
       };
 
-  if(args.flags.help) {
-    return cb(null, utils.help('doc/help/mkfilter.txt'));
-  }else if(args.flags.version) {
-    return cb(null, utils.version(pkg));
-  }
+  prg.run(argv, runtime, function parsed(err) {
+    if(err) {
+      return cb(err); 
+    }
 
-  var k
-    , type;
+    var k
+      , type;
 
-  // check args for switches and update filters
-  for(k in keys) {
-    if(args.flags[k]) {
-      type = keys[k];
-      filters[type] = true;
-    } 
-  }
+    // check args for switches and update filters
+    for(k in keys) {
+      if(this[k] === true) {
+        type = keys[k];
+        filters[type] = true;
+      } 
+    }
 
-  // invert flags
-  if(args.flags.invert) {
+    // invert flags
+    if(this.invert) {
+      for(k in filters) {
+        filters[k] = !filters[k];
+      } 
+    }
+
+    // assign final values
     for(k in filters) {
-      filters[k] = !filters[k];
-    } 
-  }
+      this[k] = filters[k];
+    }
 
-  for(k in filters) {
-    opts[k] = filters[k];
-  }
-
-  filter(opts, cb);
+    filter(this, cb);
+  })
 }
 
-module.exports = cli;
+module.exports = main;
