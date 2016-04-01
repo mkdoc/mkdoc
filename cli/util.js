242c4dab1596a93d9c1763d5b6bfceda4710c13f
muji <freeformsystems@gmail.com>
2016-04-01 20:26:31 +0800

    Assert on util functions.

--- cli/util.js
+++ cli/util.js
@@ -1,11 +1,12 @@
 function error(msg) {
   var prefix = 'ERR | ';
-  console.error(prefix + msg);
-  process.exit(1);
+  process.stdout.write(prefix + msg + '\n');
+  process.exitCode = 1;
 }
 
 function finish(err) {
   if(err) {
+    /* istanbul ignore next: an error with no message can happen */
     error(err.message || err.stack); 
   }
 }
---
+++ test/spec/util.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect
+  , util = require('../../cli/util');
+
+describe('util:', function() {
+
+  it('should ignore with no error', function(done) {
+    util.finish();
+    done();
+  });
+
+  it('should write error message', function(done) {
+    var writer = process.stdout.write;
+    function intercept(buf) {
+      expect(buf).to.eql('ERR | mock error\n');
+    }
+
+    process.stdout.write = intercept;
+    util.finish(new Error('mock error'));
+    process.stdout.write = writer;
+    done();
+  });
+
+});
