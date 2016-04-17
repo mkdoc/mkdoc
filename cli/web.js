f4bd48fa1ff8770aee33fd7bea1cd2e11f558c36
muji <freeformsystems@gmail.com>
2016-04-17 17:02:20 +0800

    Sketch mkweb program.

---
+++ cli/web.js
@@ -0,0 +1,71 @@
+var path = require('path')
+  , page = require('mkpage')
+  , cli = require('mkcli')
+  , pkg = require('mkpage/package.json')
+  , prg = cli.load(require('../doc/json/mkweb.json'));
+
+/**
+ *  @name mkweb
+ *  @cli doc/cli/mkweb.md
+ */
+function main(argv, conf, cb) {
+
+  /* istanbul ignore if: always pass argv in test env */
+  if(typeof argv === 'function') {
+    cb = argv;
+    argv = null;
+  }
+
+  /* istanbul ignore if: always pass conf in test env */
+  if(typeof conf === 'function') {
+    cb = conf;
+    conf = null;
+  }
+
+  /* istanbul ignore next: always pass conf in test env */
+  conf = conf || {};
+  /* istanbul ignore next: never print to stdout in test env */
+  conf.output = conf.output || process.stdout;
+  /* istanbul ignore next: never read from stdin in test env */
+  conf.input = conf.input || process.stdin;
+
+  var opts = {
+      input: conf.input,
+      output: conf.output
+    }
+    , runtime = {
+        base: path.normalize(path.join(__dirname, '..')),
+        target: opts,
+        hints: prg,
+        multiple: prg,
+        help: {
+          file: 'doc/help/mkweb.txt',
+          output: conf.output
+        },
+        version: {
+          name: 'mkweb',
+          version: pkg.version,
+          output: conf.output
+        },
+        plugins: [
+          require('mkcli/plugin/epipe'),
+          require('mkcli/plugin/hints'),
+          require('mkcli/plugin/argv'),
+          require('mkcli/plugin/help'),
+          require('mkcli/plugin/version'),
+          require('mkcli/plugin/multiple')
+        ]
+      };
+
+  cli.run(prg, argv, runtime, function parsed(err, req) {
+    if(err || req.aborted) {
+      return cb(err); 
+    }
+
+    page(this, cb);
+  })
+}
+
+main.pkg = pkg;
+
+module.exports = main;
---
+++ doc/cli/mkweb.md
@@ -0,0 +1,35 @@
+# Name
+
+mkweb - build a website
+
+# Synopsis
+
+```
+[options] ... <file|dir>
+```
+
+# Options
+
+* `-o, --output=<DIR>` Write files to DIR
+* `-c, --conf=[FILE] :file:_files '*.yml'` Read YAML configuration from FILE
+* `-p, --pi` Execute processing instructions
+* `-s, --safe` Safe mode for instruction execution
+* `-b, --bundle=[FILE...]` Files to add to the asset bundle
+* `-t, --theme=[VAL]` Use stylesheet theme
+* `-f, --frontmatter` Parse inline frontmatter YAML
+* `-h, --help` Display help and exit
+* `--version` Print the version and exit
+
+# Description
+
+Reads input markdown documents and compiles each document to an HTML page, files are written to the directory specified using the `--output` option.
+
+# Configuration
+
+Set the global configuration using the `--conf` option if a file exists with a `.md.yml` extension it is used as a configuration specific to the file. If the `--frontmatter` option is specified inline YAML is parsed from the input documents; in this case the YAML must be at the beginning of the file (no preceeding characters).
+
+For available configuration options see the documentation for mkpage(1).
+
+# See Also
+
+mkpage(1)
